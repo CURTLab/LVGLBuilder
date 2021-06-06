34606300e3c8c7e9e3069c21589ae11493037d07
qzs <qzs@vip.126.com>
2021-06-06 17:19:43 +0800

    google breakpad will only be used in window

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include <QApplication>
 
 #include "MainWindow.h"
-#include "breakpad/client/windows/handler/exception_handler.h"
 //#include "vld.h"
+#ifdef Q_OS_WIN
+#include "breakpad/client/windows/handler/exception_handler.h"
 
 bool callback(const wchar_t *dump_path, const wchar_t *id, void *context,
               EXCEPTION_POINTERS *exinfo, MDRawAssertionInfo *assertion,
               bool succeeded) {
   return succeeded;
 }
+#endif
 
 int main(int argc, char *argv[]) {
+#ifdef Q_OS_WIN
   google_breakpad::ExceptionHandler eh(
       L".", NULL, callback, NULL,
       google_breakpad::ExceptionHandler::HANDLER_ALL);
+#endif
   QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QApplication a(argc, argv);
 
